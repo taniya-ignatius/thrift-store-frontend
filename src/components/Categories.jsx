18c9a40756eac5ade06f04652e7d3c696c07a018
taniya-ignatius <itaniya895@gmail.com>
2024-02-14 11:34:42 +0530

    Horror added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -14,12 +14,36 @@ const Categories = () => {
                                 </div>
                         </div>
                     </div>
-                    <div className="col col-6 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
-                    <div className="col col-6 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
-                    <div className="col col-6 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+                    <div className="col col-6 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                    <div class="card">
+                            <img src="https://www.icegif.com/wp-content/uploads/icegif-4799.gif" height="250px" width="250px" alt="..."/>
+                                <div class="card-body">
+                                   <center><h4 class="card-title">ROMANCE</h4></center> 
+                                   <center> <a href="#" class="btn btn-primary">EXPLORE</a></center>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="col col-6 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                    <div class="card">
+                    <img src="https://giffiles.alphacoders.com/166/16607.gif" height="250px" width="250px" alt="..."/>
+                                <div class="card-body">
+                                   <center><h4 class="card-title">FICTION</h4></center> 
+                                   <center> <a href="#" class="btn btn-primary">EXPLORE</a></center>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="col col-6 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                    <div class="card">
+                    <img src="https://media2.giphy.com/media/l1J9HWBKLp20YfNAY/200w.gif?cid=6c09b952u2oibb33a4ce99kusvm16cw28ny97mp80hmx2spj&ep=v1_gifs_search&rid=200w.gif&ct=g" height="250px" width="250px" alt="..."/>
+                                <div class="card-body">
+                                   <center><h4 class="card-title">THRILLER</h4></center> 
+                                   <center> <a href="#" class="btn btn-primary">EXPLORE</a></center>
+                                </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+            </div>
     )
 }
 
