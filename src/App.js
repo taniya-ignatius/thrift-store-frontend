6ed63200e3977da72a172e91bcfac01056807bf2
taniya-ignatius <itaniya895@gmail.com>
2024-02-14 11:21:01 +0530

    Horror added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Carousal from './components/Carousal';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <div>
       <NavBar/>
-      <Carousal/>
+      <Carousal/><br></br>
+      <Categories/>
     </div>
   );
 }
